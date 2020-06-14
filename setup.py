9821bc14dc6f4de6107d8ff235f8f1dcaf5f5af8
andystewart999 <andy.stewart@live.com>
2020-06-14 18:24:47 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyTransportNSW",
-    version="0.1.3",
+    version="0.2.0",
     author="andystewart999",
     description="Get transport information from TransportNSW",
     long_description=long_description,
